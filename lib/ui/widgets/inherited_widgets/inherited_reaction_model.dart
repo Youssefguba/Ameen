78cc2823d2ca5118eeed1329f7d06f631bf723c0
Youssefguba <youssefguba6@gmail.com>
2020-06-26 19:20:51 +0200

    Handle Profile && Notification Page && Search Page & Following Features

---
+++ lib/ui/widgets/inherited_widgets/inherited_reaction_model.dart
@@ -0,0 +1,19 @@
+//import 'package:flutter/material.dart';
+//
+//
+//class InheritedReactionData extends InheritedWidget {
+//  Widget child;
+//  InheritedReactionData({Key key, this.ameenReaction, this.child}) : super(key: key);
+//
+//  static InheritedReactionData of(BuildContext context ){
+//    // ignore: deprecated_member_use
+//    return(context.inheritFromWidgetOfExactType(InheritedReactionData) as InheritedReactionData);
+//  }
+//
+//  @override
+//  bool updateShouldNotify(InheritedWidget oldWidget) {
+//    // TODO: implement updateShouldNotify
+//    return true;
+//  }
+//
+//}
